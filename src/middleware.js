32135d68248016cdabf36956bbf05b1417484e41
son vongsavath <phoukeochittanavanh@sonvongsavathmacbook.local>
2026-01-24 21:56:58 +0700

    Fix loi middleware unsupported modules

--- src/middleware.js
+++ src/middleware.js
@@ -4,9 +4,9 @@ export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // Bỏ qua các file nội bộ của Next.js và file tĩnh
+    // Bỏ qua các file nội bộ của Next.js và các file tĩnh
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Luôn chạy cho các API routes
+    // Luôn chạy middleware cho các API
     '/(api|trpc)(.*)',
   ],
 };
